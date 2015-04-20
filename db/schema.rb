a1c349035884292cde03b4c78b7a00a928b529b7
Brent Haas <brenthaas@gmail.com>
2015-04-20 13:34:44 -0700

    created Tag model

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150420201753) do
+ActiveRecord::Schema.define(version: 20150420202549) do
 
   create_table "entities", force: :cascade do |t|
     t.string   "type"
     t.string   "identifier"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name",       null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
